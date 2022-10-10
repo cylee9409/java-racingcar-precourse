55409e7dbbef41979c6e69abf9716af69a1f8d26
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-10-10 20:21:42 +0900

    feat : Car 클래스의 일급 콜렉션인 Cars 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars() {
+        cars = new ArrayList<>();
+    }
+}
