cddcb76181110384c038a6ce7286c0efd19c1a50
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-10-10 21:58:13 +0900

    fix : 클래스명 오탈자 수정
    - RacingCarTest -> RacingCntTest 변경

--- src/test/java/racingcar/domain/RacingCntTest.java
+++ src/test/java/racingcar/domain/RacingCntTest.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.Application;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 
-class RacingCarTest extends NsTest {
+class RacingCntTest extends NsTest {
     @ParameterizedTest
     @ValueSource(strings = {"abc", " ", "9df92"})
     void 레이싱_횟수_숫자_타입_아닌_값_예외_처리_테스트(String input) {
